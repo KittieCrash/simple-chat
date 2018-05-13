7680cecdb9f8df8da85d30f4f45518d772a452f9
Kittie Durkey <kittie.durkey@gmail.com>
2018-05-13 03:10:03 -0700

    Adds websocket secure

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -2,7 +2,7 @@ import * as types from '../constants/ActionTypes'
 import { addUser, messageReceived, populateUsersList } from '../actions'
 
 const setupSocket = (dispatch, username) => {
-  const socket = new WebSocket('wss://kittie-chat.herokuapp.com:8989')
+  const socket = new WebSocket('wss://34.237.148.235:8989')
 
   socket.onopen = () => {
     socket.send(JSON.stringify({
