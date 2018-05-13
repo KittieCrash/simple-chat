d515f8fda6a0f1f7a3b36d3b17bd4bf5779930ba
Kittie Durkey <kittie.durkey@gmail.com>
2018-05-13 03:19:25 -0700

    Adds websocket secure

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -2,7 +2,9 @@ import * as types from '../constants/ActionTypes'
 import { addUser, messageReceived, populateUsersList } from '../actions'
 
 const setupSocket = (dispatch, username) => {
-  const socket = new WebSocket('wss://34.237.148.235:8989')
+  var HOST = location.origin.replace(/^http/, 'ws')
+
+  const socket = new WebSocket(HOST)
 
   socket.onopen = () => {
     socket.send(JSON.stringify({
